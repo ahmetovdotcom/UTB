9539c98884b2f3e368dc927f26ace07092a09fe0
Amir Ahmetov <amirahmetov@192.168.10.3>
2026-09-08 18:05:11 +0500

    fixed build error

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { registerSW } from 'virtual:vite-plugin-pwa'
+import { registerSW } from 'virtual:pwa-register'
 
 
 registerSW({ immediate: true })
